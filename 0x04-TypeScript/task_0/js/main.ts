9e57569d3ea56653fa406dbac3374c1871231773
Emmaobi7 <emma46574@gmail.com>
2023-12-06 13:03:22 +0000

    changid env

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,23 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+  };
+
+const student0: Student = {
+  firstName: 'Emma',
+  lastName: 'okenwa',
+  age: 22,
+  location: 'zaria',
+  };
+
+const student1: Student = {
+  firstName: 'obinna',
+  lastName: 'okenwa',
+  age: 23,
+  location: 'kano',
+  };
+
+let studentsList = [student0, student1];
+
