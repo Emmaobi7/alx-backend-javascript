f5ae87efef22b335e105e9591c80ca2a6a98a3b7
Emmaobi7 <emma46574@gmail.com4>
2024-02-01 15:54:10 +0100

    made changes to files

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -4,7 +4,6 @@ const request = require('request')
 
 describe('test for / get route', function() {
   it('should test for correct status', function(done) {
-    this.timeout(0)
     request.get('http://localhost:7865/', function(err, res, body) {
       expect(res.statusCode).to.be.equal(200);
       expect(res.body).to.be.equal('Welcome to the payment system');
@@ -13,7 +12,6 @@ describe('test for / get route', function() {
   })
 
   it('/ route only', function(done) {
-    this.timeout(0)
     request.get('http://localhost:7865/..', function(err, res, body) {
       expect(res.statusCode).to.be.equal(404)
       done();
@@ -24,7 +22,6 @@ describe('test for / get route', function() {
 
 describe('Test for cart page', function() {
   it('correct statuscode', function(done) {
-    this.timeout(0)
     request.get('http://localhost:7865/cart/2', function(err, res, body) {
       expect(res.statusCode).to.be.equal(200)
       expect(res.body).to.be.equal('Payment methods for cart 2')
@@ -33,7 +30,6 @@ describe('Test for cart page', function() {
   })
 
   it('correst statuscode', function(done) {
-    this.timeout(0)
     request.get('http://localhost:7865/cart/-2', function(err, res, body) {
       expect(res.statusCode).to.be.equal(404)
       done();
@@ -41,7 +37,6 @@ describe('Test for cart page', function() {
   })
 
   it('correct statuscode', function(done) {
-    this.timeout(0)
     request.get('http://localhost:7865/cart/2ewcu34cb', function(err, res, body) {
       expect(res.statusCode).to.be.equal(404)
       done();
@@ -52,7 +47,6 @@ describe('Test for cart page', function() {
 
 describe('Test for /login', function() {
   it('test status code', function(done) {
-      this.timeout(0)
       request.post('http://localhost:7865/login', {json: {userName: 'Betty'}}, function(err, res, body) {
         expect(res.statusCode).to.be.equal(200)
         expect(body).to.be.equal(`Welcome :Betty`)
@@ -64,7 +58,6 @@ describe('Test for /login', function() {
 
 describe('Test for /available_payments', function() {
   it('test for status code and correct object', function(done) {
-    this.timeout(0)
     request.get('http://localhost:7865/available_payments', function(err, res, body) {
       expect(res.statusCode).to.be.equal(200)
       expect(JSON.parse(body)).to.be.deep.equal({payment_methods: {credit_cards: true, paypal: false }})
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -4,7 +4,6 @@ const request = require('request')
 
 describe('test for / get route', function() {
   it('should test for correct status', function(done) {
-    this.timeout(0)
     request.get('http://localhost:7865/', function(err, res, body) {
       expect(res.statusCode).to.be.equal(200);
       expect(res.body).to.be.equal('Welcome to the payment system');
@@ -13,7 +12,6 @@ describe('test for / get route', function() {
   })
 
   it('/ route only', function(done) {
-    this.timeout(0)
     request.get('http://localhost:7865/..', function(err, res, body) {
       expect(res.statusCode).to.be.equal(404)
       done();
